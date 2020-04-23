41cbfeb79c1adb7232f0c8671e60113b2babf6db
SimoneTesta1998 <simone3000.st@gmail.com>
2020-04-24 01:24:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-1o4jdp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-1o4jdp)
---
+++ src/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {StoreModule} from "@ngrx/store";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {productReducer} from "./store/reducers/products.reducer";
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({
+      products: productReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
